a2806f1a1583c2434d2b407b58eff1a3bffd2df9
dimi11433 <dimitricromarty@gmail.com>
2025-04-12 18:55:53 -0400

    Working on the bit maps

--- generate_asteroid.vhd
+++ generate_asteroid.vhd
@@ -20,22 +20,69 @@ architecture asteroids of asteroid_gen is
     constant SCREEN_HEIGHT : integer := 480;
 
     constant ASTEROID_SIZE_2 : integer := 10;
-    constant ASTEROID_SIZE_3 : integer := 20;
-    constant ASTEROID_SIZE_4 : integer := 30;
-    constant ASTEROID_SIZE_5 : integer := 40;
+    constant ASTEROID_SIZE_3 : integer := 15;
+    constant ASTEROID_SIZE_4 : integer := 20;
+    constant ASTEROID_SIZE_5 : integer := 25;
 
     constant ASTEROID_DY : integer := 1;
     constant ASTEROID_DX : integer := 1;
 
     signal pix_x, pix_y : unsigned(9 downto 0);
+
     signal asteroid_rom_bit : std_logic_vector(1 downto 0);
 
-    signal asteroid_x_start, asteroid_x_end : unsigned(9 downto 0);
-    signal asteroid_y_top, asteroid_y_bottom : unsigned(9 downto 0);
+    type asteroid_id is record 
+        asteroid_x_start : unsigned(9 downto 0);
+        asteroid_x_end : unsigned(9 downto 0);
+        asteroid_y_top : unsigned(9 downto 0);
+        asteroid_y_bottom : unsigned(9 downto 0);
+    end record asteroid_id;
+
+    type asteroid_mov is record
+        asteroid_x_start_next : unsigned(9 downto 0);
+        asteroid_y_top_next : unsigned(9 downto 0);
+    end record asteroid_mov;
+
+    --we create an array of records which store the asteroids size aka ID
+    type  asteroid_id_arry is array (1 downto 0) of asteroid_id;
+    --create an array of records which store the movements of each asteroid
+    type asteroid_mov_arry is array (1 downto 0) of asteroid_mov;
+
+    signal asteroid_on : std_logic_vector(1 downto 0);
+
+    signal asteroid_colour : std_logic_vector(2 downto 0);
+
+    signal refresh_screen : std_logic;
+
+    signal collision_with_asteroid, collision_with_alien : std_logic;
+
+    signal collision_with_asteroid_happened : std_logic;
+
+    --asteroid image
+    type rom_type_10 is array(0 downto 9) of std_logic_vector(0 downto 9);
+    constant ASTEROID_ROM_1 : rom_type_10 := (
+        "0011111100",
+        "0111111110",
+        "1111111111",
+        "1111111111",
+        "1111111111",
+        "1111111111",
+        "1111111111",
+        "1111111111",
+        "0111111110",
+        "0011111100"
+    );
+
+    type rom_type_15 is array(0 downto 14) of std_logic_vector(0 downto 14);
+    constant ASTEROID_ROM_2 : rom_type_15 := (
 
 
-    -- next start positions for the objects
-    signal asteroid_x_start_next, asteroid_y_top_next : unsigned(9 downto 0);
+
+
+
+    
+    );
+
 
 
 
